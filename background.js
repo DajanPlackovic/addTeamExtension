e82bfc2a18dc1b2119c742a30a7bcff0ba067862
Dajan Plackovic <118908110+DajanPlackovic@users.noreply.github.com>
2025-05-29 09:58:08 +0000

    add url check

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.id) {
+  if (tab.id && tab.url) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ['script.js'],
