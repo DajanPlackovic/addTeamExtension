4fc0439df675a3f0c00913d28e72e7cb9eb097a2
Dajan Plackovic <118908110+DajanPlackovic@users.noreply.github.com>
2025-05-30 13:18:05 +0000

    i'm a fucking idiot

--- background.js
+++ background.js
@@ -1,21 +1,6 @@
-// const tracker = {
-//   tabIds: new Set(),
-//   addTabId: function(tabId) {
-//     this.tabIds.add(tabId);
-//     setTimeout(function () {
-//       this.tabIds.remove(tabId);
-//     }, 1000)
-//   }
-// }
-
 chrome.action.onClicked.addListener(async (tab) => {
-  console.log(`did the thing`);
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: [ "script.js" ]
+  });
 });
-
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.type === "ADDING_COMPLETE") {
-//     completed.add(message.url)
-//     console.log(completed);
-//   }
-//   sendResponse({ status: "success", receivedAt: new Date().toISOString() });
-// })
